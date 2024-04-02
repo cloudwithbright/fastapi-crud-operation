57e66db00db4cc1c13e307819a101ccabc1150bb
Owusu Bright Debrah <74520811+cloudwithdeb@users.noreply.github.com>
2024-04-02 11:33:49 +0000

    Update main.py

--- application/app/main.py
+++ application/app/main.py
@@ -114,13 +114,13 @@ async def delete_item(item_id: int, db: Session = Depends(get_db)):
     return item
 
 
-if __name__ == "__main__":
-    print("Starting webserver...")
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8080)),
-        debug=os.getenv("DEBUG", False),
-        log_level=os.getenv('LOG_LEVEL', "info"),
-        proxy_headers=True
-    )
+# if __name__ == "__main__":
+#     print("Starting webserver...")
+#     uvicorn.run(
+#         app,
+#         host="0.0.0.0",
+#         port=int(os.getenv("PORT", 8080)),
+#         debug=os.getenv("DEBUG", False),
+#         log_level=os.getenv('LOG_LEVEL', "info"),
+#         proxy_headers=True
+#     )
